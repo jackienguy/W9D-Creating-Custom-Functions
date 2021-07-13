34d45abed0de400e7178a6663deffd391ab7eb8a
Your Name <you@example.com>
2021-07-12 21:41:28 -0600

    completed function 1 and 2

---
+++ javascript/apps.js
@@ -0,0 +1,22 @@
+// Function 1
+function math(numOne, numTwo) {
+    var sum = numOne + numTwo;
+    return sum*5;
+}   
+console.log(math(2,3));
+
+// Function 2
+var string = "hakuna matata";
+
+function chars (letters) {
+    var numCharacters = 12;
+    if (numCharacters >=10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(chars(string));
+
+// Function 3
+
